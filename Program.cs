00d9aa8b2afc2f98bf6302c19390d48a08d07c3d
mizanussayed <mizandusp@gmail.com>
2023-07-13 16:03:31 +0600

    Version 2 for Nginx

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
-var app = builder.Build();
+builder.Services.Configure<ForwardedHeadersOptions>(option => {
+option.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
 
+var app = builder.Build();
+app.UseForwardedHeaders();
 // Configure the HTTP request pipeline.
 // if (!app.Environment.IsDevelopment())
 // {
@@ -20,8 +29,41 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
+
+ 
+app.UseEndpoints(endpoints =>
+{
+    app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+    
+    endpoints.MapGet("/log", async context =>
+    {
+        context.Response.ContentType = "text/plain";
 
+        // Host info
+        var name = Dns.GetHostName(); // get container id
+        var ip = Dns.GetHostEntry(name).AddressList.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
+        Console.WriteLine($"Host Name: { Environment.MachineName} \t {name}\t {ip}");
+        await context.Response.WriteAsync($"Host Name: {Environment.MachineName}{Environment.NewLine}");
+        await context.Response.WriteAsync(Environment.NewLine);
+
+        // Request method, scheme, and path
+        await context.Response.WriteAsync($"Request Method: {context.Request.Method}{Environment.NewLine}");
+        await context.Response.WriteAsync($"Request Scheme: {context.Request.Scheme}{Environment.NewLine}");
+        await context.Response.WriteAsync($"Request URL: {context.Request.GetDisplayUrl()}{Environment.NewLine}");
+        await context.Response.WriteAsync($"Request Path: {context.Request.Path}{Environment.NewLine}");
+
+        // Headers
+        await context.Response.WriteAsync($"Request Headers:{Environment.NewLine}");
+        foreach (var (key, value) in context.Request.Headers)
+        {
+            await context.Response.WriteAsync($"\t {key}: {value}{Environment.NewLine}");
+        }
+        await context.Response.WriteAsync(Environment.NewLine);
+
+        // Connection: RemoteIp
+        await context.Response.WriteAsync($"Request Remote IP: {context.Connection.RemoteIpAddress}");
+    });
+});
 app.Run();
